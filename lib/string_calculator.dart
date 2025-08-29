3e909c64e9501afd9c0d136b2c9896778425a336
Sudhanshu <11.shaurya@gmail.com>
2025-08-29 15:23:56 +0530

    refactor: extract delimiters regex into a variable for clarity

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,9 +1,10 @@
 class StringCalculator {
   int add(String numbers) {
+    final delimiters = RegExp('[,\\n]');
     if (numbers.isEmpty) {
       return 0;
     }
-    final List<String> numListString = numbers.split(RegExp('[,\\n]')).toList();
+    final List<String> numListString = numbers.split(delimiters).toList();
     final List<int> numList = numListString.map(int.parse).toList();
     return numList.reduce((a, b) => a + b);
   }

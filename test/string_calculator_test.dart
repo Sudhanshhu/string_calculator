e06239725eb8fa84d35247f3d3ac30694320ca28
Sudhanshu <11.shaurya@gmail.com>
2025-08-29 13:17:05 +0530

    return sum of two number separated by comma in progress

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -4,12 +4,30 @@ import 'package:test/test.dart';
 void main() {
   group('StringCalculator', () {
     final calculator = StringCalculator();
-    test('returns 0 for empty string', () {
-      expect(calculator.add(""), equals(0));
-    });
+    test(
+      'returns 0 for empty string',
+      () {
+        expect(calculator.add(""), equals(0));
+      },
+    );
 
-    test('returns number itself for a single number string', () {
-      expect(calculator.add("1"), equals(1));
-    });
+    test(
+      'returns number itself for a single number string',
+      () {
+        expect(calculator.add("1"), equals(1));
+      },
+    );
+
+    test(
+      "return sum of two number separated by comma",
+      () {
+        expect(
+          calculator.add("2,5"),
+          equals(7),
+        );
+      },
+    );
+
+    // End
   });
 }

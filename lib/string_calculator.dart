626f1ce10610d8471ed871ee8e47e7593ca6dba1
Sudhanshu <11.shaurya@gmail.com>
2025-08-29 17:40:14 +0530

    refactor: update delimiter handling to support dynamic delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,10 +1,21 @@
 class StringCalculator {
   int add(String numbers) {
-    final delimiters = RegExp('[,\\n]');
     if (numbers.isEmpty) {
       return 0;
     }
-    final List<String> numListString = numbers.split(delimiters).toList();
+
+    final List<String> delimiters = [',', '\n'];
+    String numString = numbers;
+
+    if (numbers.contains("//")) {
+      final delimiterTillIndex = numbers.indexOf("\n");
+      final String delimitter = numbers.substring(2, delimiterTillIndex);
+      delimiters.add(delimitter);
+      numString = numbers.substring(delimiterTillIndex + 1);
+    }
+    final pattern = delimiters.map(RegExp.escape).join('|');
+    final List<String> numListString =
+        numString.split(RegExp(pattern)).toList();
     final List<int> numList = numListString.map(int.parse).toList();
     return numList.reduce((a, b) => a + b);
   }
